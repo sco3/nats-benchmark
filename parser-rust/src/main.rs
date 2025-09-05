c8348c3b48a3f7f77c496bf3c3ecd288b78a8ca4
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-09-05 20:34:03 +0100

    parser

--- parser-rust/src/main.rs
+++ parser-rust/src/main.rs
@@ -1,6 +1,16 @@
+use clap::Parser;
+use regex::Regex;
 use std::fs;
 use std::path::Path;
-use regex::Regex;
+
+#[derive(Parser)]
+#[command(name = "parser")]
+#[command(version = "0.1.0")]
+#[command(about = "Parses nats benchmark logs", long_about = None)]
+struct Cli {
+    #[arg(long, short, default_value = ".")]
+    path: String,
+}
 
 #[derive(Debug)]
 struct LogData {
@@ -10,31 +20,34 @@ struct LogData {
 }
 
 fn parse_log_file(file_path: &Path) -> String {
-    let content = fs::read_to_string(file_path).unwrap();
-    let re = Regex::new(r"(\d{1,3}(?:,\d{3})*|\d+)\s+msgs/sec").unwrap();
-    if let Some(captures) = re.captures(&content) {
-        if let Some(match_str) = captures.get(1) {
-            return match_str.as_str().replace(",", "");
+    if let Ok(content) = fs::read_to_string(file_path) {
+        if let Ok(re) = Regex::new(r"(\d{1,3}(?:,\d{3})*|\d+)\s+msgs/sec") {
+            if let Some(captures) = re.captures(&content) {
+                if let Some(match_str) = captures.get(1) {
+                    return match_str.as_str().replace(",", "");
+                }
+            }
         }
     }
-    "N/A".to_string()
+    "".to_string()
 }
 
 fn get_sort_key(dir_name: &str) -> u64 {
     if let Some(val_str) = dir_name.strip_suffix("ms") {
-        val_str.parse().unwrap()
+        val_str.parse().unwrap_or_default()
     } else if let Some(val_str) = dir_name.strip_suffix("s") {
-        val_str.parse::<u64>().unwrap() * 1000
+        val_str.parse::<u64>().unwrap_or_default() * 1000
     } else if let Some(val_str) = dir_name.strip_suffix("m") {
-        val_str.parse::<u64>().unwrap() * 60 * 1000
+        val_str.parse::<u64>().unwrap_or_default() * 60 * 1000
     } else {
         u64::MAX
     }
 }
 
 fn main() {
+    let cli = Cli::parse();
     let mut data = Vec::new();
-    for entry in fs::read_dir(".").unwrap() {
+    for entry in fs::read_dir(cli.path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
@@ -62,4 +75,3 @@ fn main() {
         println!("| {} | {} | {} |", d.dir_name, d.pub_val, d.sub_val);
     }
 }
-
